8090a1b3c96eaf2f6fb54ad26ad7031e2d2009bc
bruhmux <fordashton3@gmail.com>
2026-02-26 00:19:40 -0600

    remove routes mod

--- server/src/routes.rs
+++
@@ -1,9 +0,0 @@
-use axum::response::IntoResponse;
-
-pub async fn root() -> &'static str {
-    "API Running..."
-}
-
-pub async fn get_rooms() -> impl IntoResponse {
-    // TODO: Get room ids, corresponding room names, and host, other users stay private
-}
