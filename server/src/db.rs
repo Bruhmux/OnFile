2db56f37caa0e307e64b5cb3a7af32d74e4b2ba5
bruhmux <fordashton3@gmail.com>
2026-03-03 22:50:32 -0600

    refactor out unused and unrelated code

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,10 +1,6 @@
-use chrono::{DateTime, Utc};
-use sqlx::FromRow;
 use std::env;
-use uuid::Uuid;
 
 use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
 use sqlx::{Result, postgres::PgPoolOptions};
 
 pub async fn init_connection() -> Result<sqlx::PgPool, sqlx::Error> {
@@ -16,24 +12,4 @@ pub async fn init_connection() -> Result<sqlx::PgPool, sqlx::Error> {
         .max_connections(15)
         .connect(&db_url)
         .await
-}
-
-#[derive(FromRow)]
-pub struct DbUser {
-    pub id: Uuid,
-    pub display_name: String,
-    pub connection_token: Uuid,
-    pub connected_at: DateTime<Utc>,
-    pub last_heartbeat: DateTime<Utc>,
-}
-
-#[derive(Deserialize)]
-pub struct CreateUserRequest {
-    pub display_name: String,
-}
-
-#[derive(Serialize)]
-pub struct CreateUserResponse {
-    pub user_id: Uuid,
-    pub connection_token: Uuid,
 }
