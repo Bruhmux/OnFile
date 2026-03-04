71117e0186e6015f97bc1433c45c34f92188ab6e
bruhmux <fordashton3@gmail.com>
2026-03-03 22:48:55 -0600

    add resp/requ room structs

--- server/src/dto.rs
+++ server/src/dto.rs
@@ -2,6 +2,17 @@ use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 #[derive(Deserialize)]
+pub struct CreateRoomRequest {
+    pub display_name: String,
+}
+
+#[derive(Serialize)]
+pub struct CreateRoomResponse {
+    pub room_id: Uuid,
+    pub room_code: String,
+}
+
+#[derive(Deserialize)]
 pub struct CreateUserRequest {
     pub display_name: String,
 }
@@ -10,8 +21,4 @@ pub struct CreateUserRequest {
 pub struct CreateUserResponse {
     pub user_id: Uuid,
     pub connection_token: Uuid,
-}
-
-pub fn checkhealth() -> String {
-    "App is running".to_string()
 }
