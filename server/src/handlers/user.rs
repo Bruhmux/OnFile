3a4d28066fde5f9241f6d4fcabde9aa3574c6e13
bruhmux <fordashton3@gmail.com>
2026-03-18 02:23:21 -0500

    move user types and add creation logic

--- server/src/handlers/user.rs
+++ server/src/handlers/user.rs
@@ -1,43 +1,40 @@
+use crate::{AppState, db::User};
 use axum::{Json, extract::State, http};
-use rand::{RngExt, distr::Alphabetic};
+use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 use uuid::Uuid;
 
-use crate::AppState;
-
 #[derive(Deserialize)]
-pub struct CreateUserRequest {
-    pub display_name: String,
+struct CreateUserRequest {
+    display_name: String,
+    room_id: Uuid,
 }
 
 #[derive(Serialize)]
-pub struct CreateUserResponse {
-    pub connection_token: Uuid,
-}
-
-pub async fn set_username(Json(payload): Json<CreateUser>) -> (http::StatusCode, Json<User>) {
-    // TODO: Verify no duplicate names
-    let user = User {
-        username: payload.username,
-    };
-    (http::StatusCode::CREATED, Json(user))
-}
-
-fn generate_uuid() -> String {
-    rand::rng()
-        .sample_iter(&Alphabetic)
-        .take(5)
-        .map(char::from)
-        .collect::<String>()
-        .to_uppercase()
+struct CreateUserResponse {
+    connection_token: Uuid,
 }
 
 pub async fn create_user(
     State(state): State<Arc<AppState>>,
     Json(payload): Json<CreateUserRequest>,
-) -> Result<Json<CreateUserResponse>, axum::http::StatusCode> {
-    let uuid = generate_uuid();
-    todo!()
-    // let insert_result = query!();
+) -> (http::StatusCode, Json<CreateUserResponse>) {
+    // TODO: Verify no duplicate names
+    // TODO: SQL insertion using FromRow
+
+    let new_user = User {
+        id: Uuid::new_v4(),
+        display_name: payload.display_name,
+        connection_token: Uuid::new_v4(),
+        connected_at: Utc::now(),
+        last_heartbeat: Utc::now(),
+    };
+
+    (
+        http::StatusCode::CREATED,
+        Json(CreateUserResponse {
+            connection_token: new_user.connection_token,
+        }),
+    )
 }
