c5aa036f0c58dcc4fe3e55580c0ad65fe48a4af8
bruhmux <fordashton3@gmail.com>
2026-03-19 16:45:52 -0500

    add cli with shutdown

--- server/src/main.rs
+++ server/src/main.rs
@@ -20,24 +20,17 @@ mod user;
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let pool = init_connection().await.unwrap();
+    let (shutdown, _) = broadcast::channel::<String>(16);
+    let shutdown_cl = shutdown.clone();
 
-    let cors =
-        CorsLayer::new().allow_origin("http://localhost:5432".parse::<HeaderValue>().unwrap());
-
-    let client_static_path = "client/dist";
-    let app = Router::new()
-        // Serve bun built static files
-        .fallback_service(ServeDir::new(client_static_path))
-        .layer(cors)
-        .route("/", get(checkhealth()));
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on {addr}");
-
-    let listener = tokio::net::TcpListener::bind(addr)
+    let pool: PgPool = init_connection()
         .await
-        .expect("Failed to bind");
+        .expect("Failed to connect to database");
+    let app_state = Arc::new(AppState { db: pool });
 
-    axum::serve(listener, app).await.expect("Server Error");
+    //     Server Start
+    let server_task = serve_app(app_state.clone());
+    let cli_task = cli_loop(State(app_state.clone()));
+
+    join!(server_task, cli_task);
 }
