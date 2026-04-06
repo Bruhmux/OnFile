0d4d4ea9c6e99be201f4b4a2edfa8a4ff720547c
bruhmux <fordashton3@gmail.com>
2026-04-06 00:53:17 -0500

    add repl

---
+++ server/src/cli/repl.rs
@@ -0,0 +1,54 @@
+use crate::{
+    db::types::AppState,
+    handlers::room::{CreateRoomRequest, create_room},
+};
+use axum::extract::State;
+use rustyline::{DefaultEditor, error::ReadlineError};
+use std::sync::Arc;
+use tokio::{
+    io::{self, AsyncBufReadExt, BufReader, Stdin},
+    task::JoinHandle,
+};
+
+pub async fn init_repl(State(state): State<Arc<AppState>>) -> JoinHandle<()> {
+    tokio::task::spawn(async move {
+        let mut repl = DefaultEditor::new().expect("Unable to initiate REPL");
+        loop {
+            let readline = repl.readline(">> ");
+            match readline {
+                Ok(line) => match line.trim() {
+                    "create room" => {
+                        let room = create_room(
+                            State(state.clone()),
+                            axum::Json(CreateRoomRequest {
+                                display_name: "cli_test".to_string(),
+                            }),
+                        )
+                        .await
+                        .unwrap();
+
+                        println!("{:?}", room);
+                    }
+                    "quit" => {
+                        println!("Shutting down...");
+                    }
+                    "status" => {
+                        println!("Running on http://127.0.0.1:3000");
+                    }
+                    "" => {}
+                    unknown => {
+                        println!("Unknown command: {unknown}.");
+                    }
+                },
+                Err(ReadlineError::Interrupted) => {
+                    println!("CTRL-C");
+                    break;
+                }
+                Err(err) => {
+                    eprintln!("Error: {:?}", err);
+                    break;
+                }
+            }
+        }
+    })
+}
