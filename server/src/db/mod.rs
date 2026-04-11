3a683ef492bd112266b7c72995060aed893f60dc
bruhmux <fordashton3@gmail.com>
2026-04-11 13:22:06 -0500

    move init to mod

--- server/src/db/mod.rs
+++ server/src/db/mod.rs
@@ -1,4 +1,14 @@
 pub mod helper;
-pub mod init;
 pub mod tables;
 pub mod types;
+
+use crate::config::Config;
+use sqlx::postgres::PgPoolOptions;
+use std::sync::Arc;
+
+pub async fn init_connection(config: Arc<Config>) -> Result<sqlx::PgPool, sqlx::Error> {
+    PgPoolOptions::new()
+        .max_connections(15)
+        .connect(config.db_url())
+        .await
+}
