1f77151e60e4dffb058716ca1134e4ed90f508c3
bruhmux <fordashton3@gmail.com>
2026-05-09 09:07:57 -0500

    fix path query

--- server/src/api/routes/ws.rs
+++ server/src/api/routes/ws.rs
@@ -1,7 +1,7 @@
-use axum::{routing::get, Router};
 use crate::api::handlers::ws;
 use crate::state::AppState;
+use axum::{Router, routing::get};
 
 pub fn router() -> Router<AppState> {
-    Router::new().route("/:room_id", get(ws::handler))
+    Router::new().route("/{room_id}", get(ws::handler))
 }
