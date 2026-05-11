7b8975a8a5d0bc0a2361bf3fe1edae5f6385b26c
bruhmux <fordashton3@gmail.com>
2026-05-11 00:49:14 -0500

    room setup is in ws

--- server/src/api/handlers/clue.rs
+++ server/src/api/handlers/clue.rs
@@ -6,10 +6,9 @@ use crate::{
 use axum::{
     Json,
     extract::{Path, State},
-    http::StatusCode,
     response::{IntoResponse, Response},
 };
-use rand::RngExt;
+use uuid::Uuid;
 
 pub async fn list(
     State(state): State<AppState>,
@@ -23,71 +22,7 @@ pub async fn list(
     Ok(Json(clues).into_response())
 }
 
-pub async fn setup_room(
-    State(state): State<AppState>,
-    Path(room_id): Path<String>,
-) -> Result<Response, AppError> {
-    let (guilty_suspect, guilty_weapon, guilty_location) = {
-        let mut rng = rand::rng();
-        (
-            rng.random_range(0..8),
-            rng.random_range(0..8),
-            rng.random_range(0..8),
-        )
-    };
-
-    // 2. Generate all clues (64x3 total relations)
-    // Simplify for now: Just generate the matches and a few mismatches
-    // Real logic: Generate entire matrix of facts
-
-    let mut transaction: sqlx::Transaction<'_, sqlx::Postgres> = state.db.begin().await?;
-
-    // Clear existing
-    sqlx::query("DELETE FROM clues WHERE room_id = $1")
-        .bind(&room_id)
-        .execute(&mut *transaction)
-        .await?;
-
-    // Insert Winning Match (S-W-L)
-    insert_clue(
-        &mut transaction,
-        &room_id,
-        Category::Suspect,
-        guilty_suspect,
-        Category::Weapon,
-        guilty_weapon,
-        true,
-    )
-    .await?;
-    insert_clue(
-        &mut transaction,
-        &room_id,
-        Category::Suspect,
-        guilty_suspect,
-        Category::Location,
-        guilty_location,
-        true,
-    )
-    .await?;
-    insert_clue(
-        &mut transaction,
-        &room_id,
-        Category::Weapon,
-        guilty_weapon,
-        Category::Location,
-        guilty_location,
-        true,
-    )
-    .await?;
-
-    // TODO: Generate the rest of the ❌ facts
-
-    transaction.commit().await?;
-
-    Ok(StatusCode::OK.into_response())
-}
-
-async fn insert_clue(
+pub async fn insert_clue(
     tx: &mut sqlx::Transaction<'_, sqlx::Postgres>,
     room_id: &str,
     x_cat: Category,
@@ -96,12 +31,14 @@ async fn insert_clue(
     y_idx: i32,
     is_true: bool,
 ) -> Result<(), AppError> {
+    let id = Uuid::new_v4();
     sqlx::query(
         r#"
         INSERT INTO clues (id, room_id, x_category, x_idx, y_category, y_idx, is_true)
         VALUES ($1, $2, $3, $4, $5, $6, $7)
         "#,
     )
+    .bind(id)
     .bind(room_id)
     .bind(x_cat)
     .bind(x_idx)
